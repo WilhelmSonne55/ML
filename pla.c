e4ea08587d57600e4407941161da42d3ad5941db
WilhelmSonne55 <r03941114@ntu.edu.tw>
2018-02-05 21:34:43 +0800

    Update pla.c

--- pla.c
+++ pla.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-#define sign(w,x) (_sign(w,x)>0?1:-1)
-#define _sign(w,x)((w[0])*(x[0])+(w[1])*(x[1])+(w[2])*(x[2])+(w[3])*(x[3]))
-
+#define sign(w,x) (_sign(w,x)>0.0?1:-1)
+#define _sign(w,x)((w[0])*(x[0])+(w[1])*(x[1])+(w[2])*(x[2])+(w[3])*(x[3])+(w[4])*(x[4]))
 
 void readFile(FILE **fptr){
 	*fptr= fopen("hw1.dat","r");		
@@ -11,39 +10,41 @@ void readFile(FILE **fptr){
 
 
 int main(){	
-	float x[4]={1,1,1,1};
-	int data=-1, y=-data;
-	int update=0, index=0, i;
-	int end = -5;
-	float w[4] = {0};
+
+	double x[5]={1};
+	double w[5] ={0};
+	
+	int y=0;
+	int update=0, index=0, error = 0, number=400;
 	FILE *file;
 	readFile(&file);
 	
 	while(1){
+		error =0;
 
-		// correct
-		if(data != y){
-	        w[0] += y*x[0]; 
-	        w[1] += y*x[1]; 
-	        w[2] += y*x[2]; 
-	        w[3] += y*x[3];
+		while(fscanf(file,"%lf%lf%lf%lf%d",&x[1],&x[2],&x[3],&x[4],&y)!=EOF){
 			
-			// printf("%f %f %f %f %d\n",w[0],w[1],w[2],w[3], y);
-
-			i=index=1;
-			++update;
-			fseek(file,0,SEEK_SET);
-		}	
-
-		if(fscanf(file, "%f%f%f%f%d",&x[0],&x[1],&x[2],&x[3],&y)==EOF)
+			if(sign(w,x) != y){
+				
+				w[0] += y*x[0]; 
+				w[1] += y*x[1]; 
+				w[2] += y*x[2]; 
+				w[3] += y*x[3];
+				w[4] += y*x[4];
+				
+				++update;
+				++error;
+			}	
+			
+			++index;	
+		}
+		if(error ==0)
 			break;
 		
-	    data = sign(w,x);	
-		++index;
+		fseek(file,0,SEEK_SET);
 	}
-
-	printf("%d %d\n",update,i);
-	printf("%d %d",data, y);
+	
+	printf("%d\n",update);
 	
 	return 0;
 }
